017c5b55ad5afe16b098b6a125d08dc35a8f295e
Abigaél Straver <31730958+AbigaelChay@users.noreply.github.com>
2017-10-26 14:36:22 +0200

    V12 Werkende Micro Interactions

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// Dit heb ik gekopieerd en aangepast van de jules.js die ik met Folkert heb gemaakt //
+"use strict";
+//Hier zeg ik welke image die waar staan worden aangeroepen //
+var allImg = document.querySelectorAll("article > div img");
+//Hier maak ik de functie die ervoor zorgt dat de images on-click worden vervangen door een eenmalig afspelend gifje, dit geldt voor alle vier (eigenlijk de drie bovenste) afbeeldingen en dat kan omdat ze allemaal als png de naam ietsUIT.png hebben en de gifjes allemaal ietsAAN.gif heten//
+function switchOneImg(afbeelding){
+    if (afbeelding.src.indexOf("UIT.png") > -1) {
+        afbeelding.src = afbeelding.src.split("UIT.png").join("AAN.gif");
+    } else if (afbeelding.src.indexOf("AAN.gif") > -1) {
+        afbeelding.src = afbeelding.src.split("AAN.gif").join("UIT.png");
+    }
+}
+
+allImg.forEach(function(element){
+    element.addEventListener("click", function(){switchOneImg(element);});
+});
