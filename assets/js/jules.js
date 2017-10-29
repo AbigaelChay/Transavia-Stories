643d7320d61619b4dab6ab754c3d1f59187d7cf7
Abigaél Straver <31730958+AbigaelChay@users.noreply.github.com>
2017-10-29 21:38:38 +0100

    V16 schoonheidsfoutjes
    Nog doen:
    - login knoppen op registreer pagina fixen
    - tekst daar iets omhoog nog ook
    - spraakwolken Jules centreren
    - mobiele versie Sheen inzetten

--- assets/js/jules.js
+++ assets/js/jules.js
@@ -7,7 +7,7 @@ var lightSwitch = document.querySelector("[data-id='lightSwitch']");
 
 var staat = 0; // staat = uit
 //Hier zeggen we dat als ie maar twee opties heeft, namelijk uit en anders aan en daaronder zeggen we dat ie dat aan moet passen als ie aangeklikt wordt //
-function switchOneImg(afbeelding){
+function switchOneImg(afbeelding) {
     if (afbeelding.src.indexOf("UIT") > -1) {
         afbeelding.src = afbeelding.src.split("UIT").join("AAN");
     } else if (afbeelding.src.indexOf("AAN") > -1) {
@@ -15,28 +15,28 @@ function switchOneImg(afbeelding){
     }
 }
 
-function switchImgsOn(afbeelding){
+function switchImgsOn(afbeelding) {
     if (afbeelding.src.indexOf("UIT") > -1) {
         afbeelding.src = afbeelding.src.split("UIT").join("AAN");
     }
 }
-function switchImgsOff(afbeelding){
+function switchImgsOff(afbeelding) {
     if (afbeelding.src.indexOf("AAN") > -1) {
         afbeelding.src = afbeelding.src.split("AAN").join("UIT");
     }
 }
 
 //En hij moet ook 180 graden draaien als ie aangeklikt wordt (uit>aan>uit>aan//
-function switchAllImg(){
-    if (staat === 0){
-        allImg.forEach(function(img){
+function switchAllImg() {
+    if (staat === 0) {
+        allImg.forEach(function (img) {
             switchImgsOn(img);
         });
         lightSwitch.style.transform = "rotate(180deg)";
         staat = 1;
         
     } else {
-        allImg.forEach(function(img){
+        allImg.forEach(function (img) {
             switchImgsOff(img);
         });
         lightSwitch.style.transform = "rotate(0deg)";
@@ -45,11 +45,11 @@ function switchAllImg(){
 }
 
 //Hier dus zeggen we dat ie "switch" on-click//
-allImg.forEach(function(element){
-    element.addEventListener("click", function(){switchOneImg(element);});
+allImg.forEach(function (element) {
+    element.addEventListener("click", function () {switchOneImg(element); });
 });
 
-lightSwitch.addEventListener("click", function(){switchAllImg();}); 
+lightSwitch.addEventListener("click", function () { switchAllImg(); }); 
             
             
             
